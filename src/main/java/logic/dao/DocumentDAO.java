3ea8634b9d54f71c05e8ebf4f3734d9a44c9fbee
Force_human <66125890+Ffferrar@users.noreply.github.com>
2022-04-17 18:26:02 +0300

    DocDAO added

--- src/main/java/logic/dao/DocumentDAO.java
+++ src/main/java/logic/dao/DocumentDAO.java
@@ -1,4 +1,43 @@
 package logic.dao;
 
+import logic.docs.Document;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class DocumentDAO {
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+
+
+    public List<Document> allDocs() {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from Document").list();
+    }
+
+    public void add(Document document){
+        Session session = sessionFactory.getCurrentSession();
+        session.persist(document);
+    }
+
+    public void delete(Document document){
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(document);
+    }
+
+    public void edit(Document document) {
+        Session session = sessionFactory.getCurrentSession();
+        session.update(document);
+    }
+
+    public Document getById(String id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Document.class, id);
+    }
 }
